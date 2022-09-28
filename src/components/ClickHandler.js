60f2527c1a26344c8b95e8aea4b5297f3f252513
nishatlea <nishataurora1956@gmail.com>
2022-09-28 18:12:57 +0600

    Clickhandler with two types of event and count number done

--- src/components/ClickHandler.js
+++ src/components/ClickHandler.js
@@ -1,11 +1,11 @@
 export const ClickHandler = () => {
-  const clickHandler = () => {
-    console.log("Button Clicked");
+  const clickHandler = (event, count = 1) => {
+    console.log("Button Clicked", event, count);
   };
   return (
     <div>
       <button onClick={clickHandler}> Click</button>
-      <button onClick={ () => clickHandler }
+      <button onClick={(event) => clickHandler(event, 5)}> Click 5 </button>
     </div>
   );
 };
