c349a9f7b5a39c10838e876e5364d8523d966214
nishatlea <nishataurora1956@gmail.com>
2022-10-19 16:12:15 +0600

    uniontype

--- dist/index.js
+++ dist/index.js
@@ -16,6 +16,19 @@ function calculateTax(income, taxYear = 2022) {
     return income * 1.3;
 }
 calculateTax(10000, 2022);
-let employee = { id: 1 };
-employee.name = "Mosh";
+let employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+kgToLbs(10);
+kgToLbs("10kg");
 //# sourceMappingURL=index.js.map
--- src/test/index.ts
+++ src/test/index.ts
@@ -38,3 +38,11 @@ let employee: Employee = {
     console.log(date);
   },
 };
+// union type
+function kgToLbs(weight: number | string): number {
+  //Narrowing
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
+kgToLbs(10);
+kgToLbs("10kg");
